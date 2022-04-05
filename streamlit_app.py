a98e6d44ebf5fd3e4160be0fe82c1326a7608e47
Tushar Pandey <56597687+pandey-tushar@users.noreply.github.com>
2022-04-05 18:04:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -168,7 +168,7 @@ def publication_analysis():
     # To access the network analysis, press the button below -
     st.write("")
     col1, col2, col3 = st.beta_columns((1,1,1))
-    link = '[Go to Department Network Analysis](https://lucky301910.github.io./)'
+    link = '[Go to Department Network Analysis](TDA_TAMIDS/dept_collab_2.html)'
     col2.markdown(link, unsafe_allow_html=True)
     # url = 'https://ritesh-suhag.github.io./'
     # if col2.button("Go to the Network Analysis"):
